78a15706c8b4540708dd4199898ade1f80f40ca7
Armen Juhl <armenjuhl@gmail.com>
2020-05-08 13:17:02 -0700

    optimize algorithm

--- split_arrays_equal_sums.py
+++ split_arrays_equal_sums.py
@@ -5,25 +5,27 @@
 # 2 arrays that have the same sum (half of the first array)
 
 
-data = [1, 3, 5, 1, 2, 6, 6, 8, 1, 5, 24, 13, 2, 3, 2, 1]
+def main():
+    data = [1, 3, 5, 1, 2, 6, 6, 8, 1, 5, 24, 13, 2, 3, 2, 1]
+    data_desc = sorted(data, key=int, reverse=True)
 
-sum_lst = sum(data)
-half_sum_lst = round(sum_lst / 2)
-data_desc = sorted(data, key=int, reverse=True)
+    output = calculate_half_sum_lst(data_desc)
+    print(f"sum of list a {sum(output[0])} \nsum of list b {sum(output[1])}")
 
 
 def calculate_half_sum_lst(data_set):
+    half_sum_lst = round(sum(data_set) / 2)
     list_a = list()
     list_b = list()
 
-    for idx, val in enumerate(data_set):
+    for val in data_set:
         if sum(list_a) + val <= half_sum_lst:
             list_a.append(val)
         else:
-            list_b.append(list_a[idx:])
+            list_b.append(val)
 
     return list_a, list_b
 
 
-output = calculate_half_sum_lst(data_desc)
-print(f"sum of list a {sum(output[0])} \nsum of list b {sum(output[1])}")
+if __name__ == '__main__':
+    main()
